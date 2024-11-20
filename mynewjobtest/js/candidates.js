6f5e4381064cc744f7f2094822535083b9985ecb
dimonsin73 <dimonsin73@gmail.com>
2024-11-20 23:51:04 +0300

    update calendar

--- mynewjobtest/js/candidates.js
+++ mynewjobtest/js/candidates.js
@@ -17,3 +17,35 @@ var output = document.querySelector('.search__range-output');
 slider.oninput = function() {
     output.innerHTML = `${this.value}%`;
 }
+
+const quantityYear = document.querySelector('.quantity__year');
+const quantityNum = document.querySelector('.quantity__num');
+quantityNum.addEventListener('input', function(){
+    if (quantityNum.value == '') {
+        quantityYear.style.display = 'none';
+    } else {
+        quantityYear.style.display = 'block';
+    };
+    
+});
+const quantityArrowMinus = document.querySelector('.quantity__arrow-minus');
+const quantityArrowPlus = document.querySelector('.quantity__arrow-plus');
+quantityArrowMinus.addEventListener('click', function(){
+    console.log(quantityNum.value)
+    if (quantityNum.value == '') {
+        quantityNum.value = 2025;
+    }
+    if (quantityNum.value > 1900) {
+        quantityNum.value--;
+    }
+    quantityYear.style.display = 'block';
+});
+quantityArrowPlus.addEventListener('click', function(){
+    if (quantityNum.value == '') {
+        quantityNum.value = 2023;
+    }
+    if (quantityNum.value < 2099) {
+        quantityNum.value++;
+    }
+    quantityYear.style.display = 'block';
+});
