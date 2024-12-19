e6305b07fb46cdd81c43ac2f2c8dff2eb7d0bb3b
dimonsin73 <dimonsin73@gmail.com>
2024-12-19 17:18:34 +0300

    19.12

---
+++ spisok_rezyume/js/spisok_rezyume.js
@@ -0,0 +1,47 @@
+const modal = document.querySelector('.modal')
+const modalWrapper = document.querySelector('.modal__wrapper')
+const cardCorrespondenceArr = document.querySelectorAll('.card__correspondence')
+for (let i = 0; i < cardCorrespondenceArr.length; i++) {
+    const element = cardCorrespondenceArr[i];
+    element.addEventListener('click', function(){
+        modal.style.display = 'flex'
+    })
+    const btnClose = modal.querySelector('.modal__head-close')
+    btnClose.addEventListener('click', function(){
+        modal.style.display = 'none'
+    })
+}
+modal.addEventListener('click', function(e){
+    const withinBoundaries = e.composedPath().includes(modalWrapper);
+    if ( ! withinBoundaries ) {
+        modal.style.display = 'none'
+    }
+})
+document.addEventListener('keydown', function(e) {
+    if( e.keyCode == 27 ){ 
+        modal.style.display = 'none'
+    }
+})
+
+// Открытие / закрытие описания вакансии
+const expandBtnArr = document.querySelectorAll('.card__btn-expand')
+for (let i = 0; i < expandBtnArr.length; i++) {
+    const element = expandBtnArr[i];
+    element.addEventListener('click', function(){
+        element.classList.toggle('btn-black-active')
+        const card = element.parentElement.parentElement
+        const expand = card.querySelector('.card__expand')
+        expand.classList.toggle('card__expand-active')
+        if (expand.classList.contains('card__expand-active')) {
+            expand.style.height = `${expand.scrollHeight}px`
+        } else {
+            expand.style.height = 0
+        }
+        const expandUp = card.querySelector('.card__expand-up')
+        expandUp.addEventListener('click', function(){
+            expand.style.height = 0
+            expand.classList.remove('card__expand-active')
+            element.classList.remove('btn-black-active')
+        })
+    })
+}
