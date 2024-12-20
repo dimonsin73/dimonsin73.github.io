e93e201989aa8d60996eda61aeb5c219e62bc1f2
dimonsin73 <dimonsin73@gmail.com>
2024-12-20 19:48:03 +0300

    20.12

--- spisok_rezyume/js/spisok_rezyume.js
+++ spisok_rezyume/js/spisok_rezyume.js
@@ -1,27 +1,32 @@
+// Открытие закрытие модального окна
 const modal = document.querySelector('.modal')
 const modalWrapper = document.querySelector('.modal__wrapper')
 const cardCorrespondenceArr = document.querySelectorAll('.card__correspondence')
 for (let i = 0; i < cardCorrespondenceArr.length; i++) {
     const element = cardCorrespondenceArr[i];
     element.addEventListener('click', function(){
+        const procent = element.querySelector('.card__correspondence-number').textContent
+        const procentModal = document.querySelector('.modal__head-number')
+        procentModal.textContent = procent
         modal.style.display = 'flex'
     })
     const btnClose = modal.querySelector('.modal__head-close')
     btnClose.addEventListener('click', function(){
         modal.style.display = 'none'
     })
+    modal.addEventListener('click', function(e){
+        const withinBoundaries = e.composedPath().includes(modalWrapper);
+        if ( ! withinBoundaries ) {
+            modal.style.display = 'none'
+        }
+    })
+    document.addEventListener('keydown', function(e) {
+        if( e.keyCode == 27 ){ 
+            modal.style.display = 'none'
+        }
+    })
 }
-modal.addEventListener('click', function(e){
-    const withinBoundaries = e.composedPath().includes(modalWrapper);
-    if ( ! withinBoundaries ) {
-        modal.style.display = 'none'
-    }
-})
-document.addEventListener('keydown', function(e) {
-    if( e.keyCode == 27 ){ 
-        modal.style.display = 'none'
-    }
-})
+
 
 // Открытие / закрытие описания вакансии
 const expandBtnArr = document.querySelectorAll('.card__btn-expand')
