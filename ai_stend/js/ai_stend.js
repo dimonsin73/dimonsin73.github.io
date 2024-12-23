d6bd9948a6efe45b0ee9baa84233d8a1bed2afac
dimonsin73 <dimonsin73@gmail.com>
2024-12-23 20:48:35 +0300

    ai stend

---
+++ ai_stend/js/ai_stend.js
@@ -0,0 +1,32 @@
+const aside = document.querySelector('.aside')
+const asideItemArr = document.querySelectorAll('.aside__item')
+
+for (let i = 0; i < asideItemArr.length; i++) {
+    const element = asideItemArr[i];
+    element.addEventListener('click', function(){
+        for (let i = 0; i < asideItemArr.length; i++) {
+            const el = asideItemArr[i];
+            el.classList.remove('aside__item-active')
+        }
+        element.classList.add('aside__item-active')
+    })
+}
+const burger = document.querySelector('.burger')
+burger.addEventListener('click', function(){
+    aside.classList.add('aside-active')
+    console.log('1')
+})
+document.addEventListener('keydown', function(e) {
+	if( e.keyCode == 27 ){ 
+		aside.classList.remove('aside-active')
+	}
+});
+document.addEventListener( 'click', (e) => {
+	const withinBoundariesAside = e.composedPath().includes(aside);
+    const withinBoundariesBurger = e.composedPath().includes(burger);
+	if ( !withinBoundariesAside) {
+        if (!withinBoundariesBurger) {
+            aside.classList.remove('aside-active')
+        }
+    }
+})
