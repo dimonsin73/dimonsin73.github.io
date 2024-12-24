43da28c2007301e061f3ad9eb3953ca289750f7b
dimonsin73 <dimonsin73@gmail.com>
2024-12-24 16:37:24 +0300

    link

--- ai_stend/js/ai_stend.js
+++ ai_stend/js/ai_stend.js
@@ -11,22 +11,3 @@ for (let i = 0; i < asideItemArr.length; i++) {
         element.classList.add('aside__item-active')
     })
 }
-const burger = document.querySelector('.burger')
-burger.addEventListener('click', function(){
-    aside.classList.add('aside-active')
-    console.log('1')
-})
-document.addEventListener('keydown', function(e) {
-	if( e.keyCode == 27 ){ 
-		aside.classList.remove('aside-active')
-	}
-});
-document.addEventListener( 'click', (e) => {
-	const withinBoundariesAside = e.composedPath().includes(aside);
-    const withinBoundariesBurger = e.composedPath().includes(burger);
-	if ( !withinBoundariesAside) {
-        if (!withinBoundariesBurger) {
-            aside.classList.remove('aside-active')
-        }
-    }
-})
