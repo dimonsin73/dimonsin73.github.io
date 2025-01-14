6348867b7c8c6dced7f95b945b7296745f343b01
dimonsin73 <dimonsin73@gmail.com>
2025-01-14 20:10:05 +0300

    Пункт 1

--- bukedo/js/script.js
+++ bukedo/js/script.js
@@ -55,13 +55,6 @@ buyer.addEventListener('click', function(){
         list.style.height = `0px`;
     }
 })
-// открытие\закрытие каталога
-const catalog = document.getElementById('catalog')
-const popup = document.querySelector('.popup')
-catalog.addEventListener('click', function(){
-    popup.classList.toggle('popup-active')
-})
-
 
 // Цветы 
 const flowers = ['Альстромерия', 'Амариллис', 'Анемон', 'Гвоздика', 'Гвоздика кустовая', 'Гербера', 'Гербера мини', 'Гиацинт', 'Гипсофила', 'Гортензия', 'Ирис', 'Калла', 'Лаванда', 'Лилия', 'Мимоза', 'Нарцисс', 'Орхидея Ванда', 'Орхидея Фаленопсис', 'Орхидея Цимбидиум', 'Пион', "Пион Сара Бернар", 'Подсолнух', 'Ранункулюс', 'Роза', 'Роза Вувузела', 'Роза Пинк Охара', 'Роза Эквадор', 'Роза кустовая', 'Роза кустовая пионовидная', "Роза пионовидная", 'Ромашка', 'Сирень', 'Тюльпан', 'Тюльпан пионовидный', 'Хлопок', 'Хризантема Антонов', 'Хризантема Момоко', 'Хризантема кустовая', 'Хризантема одноголовая', 'Эустома', 'Эустома махровая']
@@ -1010,4 +1003,359 @@ telChevrone.addEventListener('click', function(){
             telWrapper.classList.remove('tel__wrapper-active')
         }
     });
+})
+
+// Дата и время работы
+const headerLeftItem = document.querySelector('.header__left-item')
+const headerBootomSmall=document.querySelector('.header__bottom-small')
+working(headerLeftItem)
+working(headerBootomSmall)
+function working(param) {
+    const headerWorking = param.querySelector('.header__working')
+    const headerChoice = param.querySelector('.header__choice')
+    const headerChoiceSelect = param.querySelector('.header__choice-select')
+    const headerChoiceOptions = param.querySelector('.header__choice-options')
+    const headerChoiceTimeSelect = param.querySelector('.header__choice-time-select')
+    const headerChoiceTimeOptions = param.querySelector('.header__choice-time-options')
+    const headerWorkingText = param.querySelector('.header__working-text')
+    // открытие и закрытие меню
+    headerWorking.addEventListener('click', function(){
+        headerWorking.classList.toggle('header__working-active')
+        headerChoice.classList.toggle('header__choice-active')
+        document.addEventListener( 'mousedown', (e) => {
+            const withinBoundaries = e.composedPath().includes(param);
+            if ( ! withinBoundaries ) {
+                headerWorking.classList.remove('header__working-active')
+                headerChoice.classList.remove('header__choice-active')
+                headerChoiceSelect.classList.remove('header__choice-select-active')
+                headerChoiceOptions.classList.remove('header__choice-options-active')
+                headerChoiceTimeSelect.classList.remove('header__choice-time-select-active')
+                headerChoiceTimeOptions.classList.remove('header__choice-time-options-active')
+            }
+        })
+        document.addEventListener('keydown', function(e) {
+            if( e.keyCode == 27 ){ 
+                headerWorking.classList.remove('header__working-active')
+                headerChoice.classList.remove('header__choice-active')
+                headerChoiceSelect.classList.remove('header__choice-select-active')
+                headerChoiceOptions.classList.remove('header__choice-options-active')
+                headerChoiceTimeSelect.classList.remove('header__choice-time-select-active')
+                headerChoiceTimeOptions.classList.remove('header__choice-time-options-active')
+            }
+        });
+    })
+    // открытие и закрытие подменю
+    headerChoiceSelect.addEventListener('click', function(){
+        headerChoiceTimeSelect.classList.remove('header__choice-time-select-active')
+        headerChoiceTimeOptions.classList.remove('header__choice-time-options-active')
+        headerChoiceSelect.classList.toggle('header__choice-select-active')
+        headerChoiceOptions.classList.toggle('header__choice-options-active')
+    })
+    headerChoiceTimeSelect.addEventListener('click', function(){
+        headerChoiceSelect.classList.remove('header__choice-select-active')
+        headerChoiceOptions.classList.remove('header__choice-options-active')
+        headerChoiceTimeSelect.classList.toggle('header__choice-time-select-active')
+        headerChoiceTimeOptions.classList.toggle('header__choice-time-options-active')
+    })
+    // функции дней и месяцев 
+    function getWeekDay(date) {
+        let days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+        return days[date.getDay()];
+    }
+    function getMonth(month) {
+        let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+        return months[month.getMonth()];
+    }
+    // Меняем текст в хедере
+    const today = new Date()
+    headerWorkingText.textContent = `${today.getDate()} ${getMonth(today)}, с 11:00 до 21:00`
+    // построение контекта табов 
+    const headerChoiceTabArr = param.querySelectorAll('.header__choice-tab')
+    const headerChoiceOptionArr = param.querySelectorAll('.header__choice-option')
+    for (let i = 0; i < headerChoiceTabArr.length; i++) {
+        const element = headerChoiceTabArr[i];
+        const daysNumber = element.querySelector('.header__choice-number')
+        const daysDays = element.querySelector('.header__choice-days')
+        const today = new Date()
+        today.setDate(today.getDate() + i)
+        daysNumber.textContent = today.getDate()
+        if (i > 1) {
+            daysDays.textContent = getWeekDay(today)
+        }
+        element.addEventListener('click', function(){
+            for (let i = 0; i < headerChoiceTabArr.length; i++) {
+                const el = headerChoiceTabArr[i];
+                el.classList.remove('header__choice-tab-active')
+            }
+            element.classList.add('header__choice-tab-active')
+            for (let i = 0; i < headerChoiceOptionArr.length; i++) {
+                const element = headerChoiceOptionArr[i];
+                element.classList.remove('header__choice-option-active')
+            }
+            headerChoiceOptionArr[i].classList.add('header__choice-option-active')
+            headerChoiceSelect.textContent = headerChoiceOptionArr[i].textContent
+        })
+    }
+    // построение контента селектов
+    for (let i = 0; i < headerChoiceOptionArr.length; i++) {
+        const element = headerChoiceOptionArr[i];
+        const today = new Date()
+        today.setDate(today.getDate() + i)
+        switch (i) {
+            case 0:
+                element.textContent =  `Сегодня ${today.getDate()} ${getMonth(today)}`
+                break;
+            case 1:
+                element.textContent =  `Завтра ${today.getDate()} ${getMonth(today)}`
+                break;
+            default:
+                element.textContent = `${today.getDate()} ${getMonth(today)}`
+                break;
+        }
+        element.addEventListener('click', function(){
+            for (let i = 0; i < headerChoiceOptionArr.length; i++) {
+                const el = headerChoiceOptionArr[i];
+                el.classList.remove('header__choice-option-active')
+            }
+            element.classList.add('header__choice-option-active')
+            headerChoiceSelect.textContent = element.textContent
+            for (let i = 0; i < headerChoiceTabArr.length; i++) {
+                const element = headerChoiceTabArr[i];
+                element.classList.remove('header__choice-tab-active')
+            }
+            if (i < 4) {
+                headerChoiceTabArr[i].classList.add('header__choice-tab-active')
+            } 
+            headerChoiceSelect.classList.remove('header__choice-select-active')
+            headerChoiceOptions.classList.remove('header__choice-options-active')
+        })
+    }
+    // Выбор времени
+    const headerChoiceTimeOptionArr = param.querySelectorAll('.header__choice-time-option')
+    const headerChoiceTimeSelectText = param.querySelector('.header__choice-time-select-text')
+    for (let i = 0; i < headerChoiceTimeOptionArr.length; i++) {
+        const element = headerChoiceTimeOptionArr[i];
+        element.addEventListener('click', function () {
+            headerChoiceTimeSelectText.textContent = element.textContent
+            headerChoiceTimeSelect.classList.remove('header__choice-time-select-active')
+            headerChoiceTimeOptions.classList.remove('header__choice-time-options-active')
+        })
+    }
+    // Кнопка применить
+    const headerChoiceBtn = param.querySelector('.header__choice-btn')
+    headerChoiceBtn.addEventListener('click', function(){
+        headerWorkingText.textContent = `${headerChoiceSelect.textContent}, ${headerChoiceTimeSelectText.textContent}`
+        headerWorking.classList.remove('header__working-active')
+        headerChoice.classList.remove('header__choice-active')
+        headerChoiceSelect.classList.remove('header__choice-select-active')
+        headerChoiceOptions.classList.remove('header__choice-options-active')
+        headerChoiceTimeSelect.classList.remove('header__choice-time-select-active')
+        headerChoiceTimeOptions.classList.remove('header__choice-time-options-active')
+    })
+}
+
+const headerCityText = document.querySelector('.header__city-text')
+const modalCity = document.querySelector('.modal-city')
+const modalCityWrapper = modalCity.querySelector('.modal-city__wrapper')
+const modalCityClose = modalCity.querySelector('.modal-city__close')
+headerCityText.addEventListener('click', function(){
+    modalCity.classList.add('modal-city-active')
+    const modalCityItemArr = modalCity.querySelectorAll('.modal-city__item')
+    for (let i = 0; i < modalCityItemArr.length; i++) {
+        const element = modalCityItemArr[i];
+        element.addEventListener('click', function(){
+            headerCityText.textContent = element.textContent
+            modalCity.classList.remove('modal-city-active')
+        })
+    }
+    modalCityClose.addEventListener('click', function(){
+        modalCity.classList.remove('modal-city-active')
+    })
+    document.addEventListener( 'mousedown', (e) => {
+        const withinBoundaries = e.composedPath().includes(modalCityWrapper);
+        if ( ! withinBoundaries ) {
+            modalCity.classList.remove('modal-city-active')
+        }
+    })
+    document.addEventListener('keydown', function(e) {
+        if( e.keyCode == 27 ){ 
+            modalCity.classList.remove('modal-city-active')
+        }
+    });
+})
+
+const citys = ['Анапа', 'Брянск', 'Владимир', 'Волгоград', 'Воронеж', 'Геленджик', 'Дзержинск', 'Ейск', 'Екатеринбург', 'Елабуга', 'Иркутск', 'Калининград', 'Каменск-Уральский', 'Краснодар', 'Кстово', 'Курган', 'Курск', 'Москва', 'Нижнекамск', 'Нижний Новгород', 'Октябрьский', 'Омск', 'Оренбург', 'Орск', 'Пенза', 'Пермь', 'Петрозаводск', 'Пятигорск', 'Самара', 'Санкт-Петербург', 'Саратов', 'Севастополь', 'Сочи', 'Таганрог', 'Тула', 'Тюмень', 'Челябинск', 'Шахты', 'Якутск', 'Ярославль']
+const modalCityList = modalCity.querySelector('.modal-city__list')
+function createCitys(citys) {
+    for (let i = 0; i < citys.length; i++) {
+        const element = citys[i];
+        const modalCityItem = document.createElement('li')
+        modalCityItem.classList.add('modal-city__item')
+        if (element == 'Москва' || element == 'Санкт-Петербург') {
+            modalCityItem.classList.add('modal-city__item-black')
+        }
+        modalCityItem.textContent = element
+        modalCityList.append(modalCityItem)
+    } 
+}
+createCitys(citys)
+const modalCityInput = modalCity.querySelector('.modal-city__input')
+modalCityInput.addEventListener('input', function () {
+    modalCityList.innerHTML = ''
+    for (let i = 0; i < citys.length; i++) {
+        const element = citys[i];
+        const citysInput = []
+        if (element.toLowerCase().includes(modalCityInput.value.toLowerCase())) {
+            citysInput.push(element)
+        }
+        createCitys(citysInput)
+    }
+})
+
+
+// Фильтр по цене отдельный
+$(".polzunok-3").slider({
+    min: 0,
+    max: 100000,
+    values: [0, 100000],
+    range: true,
+    animate: "fast",
+    slide : function(event, ui) {    
+        $(".polzunok-input-3-left").val(ui.values[ 0 ]);   
+        $(".polzunok-input-3-right").val(ui.values[ 1 ]);  
+    }    
+});
+$(".polzunok-input-3-left").val($(".polzunok-3").slider("values", 0));
+$(".polzunok-input-3-right").val($(".polzunok-3").slider("values", 1));
+$(".polzunok-container-3 input").change(function() {
+    var input_left = $(".polzunok-input-3-left").val().replace(/[^0-9]/g, ''),    
+    opt_left = $(".polzunok-3").slider("option", "min"),
+    where_right = $(".polzunok-3").slider("values", 1),
+    input_right = $(".polzunok-input-3-right").val().replace(/[^0-9]/g, ''),    
+    opt_right = $(".polzunok-3").slider("option", "max"),
+    where_left = $(".polzunok-3").slider("values", 0); 
+    if (input_left > where_right) { 
+        input_left = where_right; 
+    }
+    if (input_left < opt_left) {
+        input_left = opt_left; 
+    }
+    if (input_left == "") {
+        input_left = 0;    
+    }        
+    if (input_right < where_left) { 
+        input_right = where_left; 
+    }
+    if (input_right > opt_right) {
+        input_right = opt_right; 
+    }
+    if (input_right == "") {
+        input_right = 0;    
+    }    
+    $(".polzunok-input-3-left").val(`${input_left.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`); 
+    $(".polzunok-input-3-right").val(`${input_right.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`); 
+    if (input_left != where_left) {
+        $(".polzunok-3").slider("values", 0, input_left);
+    }
+    if (input_right != where_right) {
+        $(".polzunok-3").slider("values", 1, input_right);
+    }
+});
+const pil3 = document.querySelector('.polzunok-input-3-left')
+const pir3 = document.querySelector('.polzunok-input-3-right')
+const p3 = document.querySelector('.polzunok-3')
+pil3.value = `${pil3.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`
+pir3.value = `${pir3.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`
+p3.addEventListener('mouseup', function(){
+    pil3.value = `${pil3.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`
+    pir3.value = `${pir3.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`
+})
+
+
+// открытие\закрытие каталога
+const catalog = document.getElementById('catalog')
+const popup = document.querySelector('.popup')
+const popupFilters = popup.querySelector('.filters')
+const popupListItemArr = popupFilters.querySelectorAll('.popup__list-item')
+const contantItemArr = popup.querySelector('.contant').children
+catalog.addEventListener('click', function(){
+    popup.classList.toggle('popup-active')
+    document.addEventListener( 'mousedown', (e) => {
+        const withinBoundaries = e.composedPath().includes(popup);
+        const withinBoundariesCatalog = e.composedPath().includes(catalog);
+        if ( ! withinBoundaries ) {
+            if (! withinBoundariesCatalog) {
+                popup.classList.remove('popup-active')
+            }
+        }
+    })
+    document.addEventListener('keydown', function(e) {
+        if( e.keyCode == 27 ){ 
+            popup.classList.remove('popup-active')
+        }
+    });
+})
+// Работа подменю  
+for (let i = 0; i < popupListItemArr.length; i++) {
+    const element = popupListItemArr[i];
+    element.addEventListener('mouseenter', function () {
+        for (let i = 0; i < popupListItemArr.length; i++) {
+            const element = popupListItemArr[i];
+            element.classList.remove('popup__list-item-active')
+        }
+        element.classList.add('popup__list-item-active')
+        for (let i = 0; i < contantItemArr.length; i++) {
+            const el = contantItemArr[i];
+            if (element.dataset.choice === el.dataset.contant) {
+                el.style.display = 'grid'
+            } else {
+                el.style.display = 'none'
+            }
+        }
+        
+    })
+}
+// построение подменю
+for (let i = 0; i < contantItemArr.length; i++) {
+    const element = contantItemArr[i];
+    switch (element.dataset.contant) {
+        case 'flowers':
+            for (let i = 0; i < flowers.length; i++) {
+                const el = flowers[i];
+                const itemLi = document.createElement('li')
+                itemLi.classList.add('contant__list-item')
+                itemLi.textContent = el
+                element.append(itemLi)
+            }
+            break;
+        case 'sizes':
+            for (let i = 0; i < sizes.length; i++) {
+                const el = sizes[i];
+                const itemLi = document.createElement('li')
+                itemLi.classList.add('contant__list-item')
+                itemLi.textContent = el
+                element.append(itemLi)
+            }
+            break;
+        case 'colors':
+            for (let i = 0; i < colors.length; i++) {
+                const el = colors[i];
+                const itemLi = document.createElement('li')
+                itemLi.classList.add('contant__list-item')
+                itemLi.textContent = el
+                element.append(itemLi)
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+const searchClue = document.querySelector('.search__clue')
+const searchInput = document.querySelector('.search__input')
+searchInput.addEventListener('input', function () {
+    searchClue.classList.add('search__clue-active')
+})
+searchInput.addEventListener('blur', function () {
+    searchClue.classList.remove('search__clue-active')
 })
