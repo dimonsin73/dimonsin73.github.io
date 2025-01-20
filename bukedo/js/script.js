5c34ba6e3102a3c4ca2ab3859451d0f1c58951c8
dimonsin73 <dimonsin73@gmail.com>
2025-01-20 21:23:30 +0300

    20/01

--- bukedo/js/script.js
+++ bukedo/js/script.js
@@ -1204,3 +1204,89 @@ for (let i = 0; i < productLinkArr.length; i++) {
     })
 }
 
+// Работа корзины
+const btnCorf = document.querySelector('.icons__corf')
+const basket = document.querySelector('.basket')
+btnCorf.addEventListener('click', function(){
+    basket.classList.add('basket-active')
+    const basketClose = basket.querySelector('.basket__close')
+    const basketWrapper = basket.querySelector('.basket__wrapper')
+    basketClose.addEventListener('click', function(){
+        basket.classList.remove('basket-active')
+    })
+    document.addEventListener( 'mousedown', (e) => {
+        const withinBoundaries = e.composedPath().includes(basketWrapper)
+        if ( ! withinBoundaries ) {
+            basket.classList.remove('basket-active')
+        }
+    })
+    document.addEventListener('keydown', function(e) {
+        if( e.keyCode == 27 ){ 
+            basket.classList.remove('basket-active')
+        }
+    });
+    // Подсчёт стоимости 
+    function priceBasket() {
+        const basketContentItemArr = basket.querySelectorAll('.basket__content-item')
+        const basketContentSum = basket.querySelector('.basket__content-sum')
+        const basketContentDelivery = basket.querySelector('.basket__content-delivery')
+        const totalPrice = basket.querySelector('.basket__content-meaning-bold')
+        let sumPrice = 0
+        for (let i = 0; i < basketContentItemArr.length; i++) {
+            const element = basketContentItemArr[i];
+            const price = Number(element.querySelector('.basket__content-price').textContent.slice(0, -1).replaceAll(' ', ''))
+            sumPrice = sumPrice + price
+        }
+        const delivery = Number(basketContentDelivery.textContent.slice(0, -1).replaceAll(' ', ''))
+        basketContentSum.textContent = `${sumPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`
+        totalPrice.textContent = `${(sumPrice+delivery).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`
+    }
+    priceBasket()
+    
+
+    // Кнопки навигации
+    const basketBtnNavArr = document.querySelectorAll('.basket__btn-nav')
+    for (let i = 0; i < basketBtnNavArr.length; i++) {
+        const element = basketBtnNavArr[i];
+        element.addEventListener('click', function(){
+            for (let i = 0; i < basketBtnNavArr.length; i++) {
+                const el = basketBtnNavArr[i];
+                el.classList.remove('basket__btn-nav-active')
+            }
+            element.classList.add('basket__btn-nav-active')
+        })
+    }
+
+    // Удаление продукта 
+    const basketContentDeleteArr = basket.querySelectorAll('.basket__content-delete')
+    for (let i = 0; i < basketContentDeleteArr.length; i++) {
+        const element = basketContentDeleteArr[i];
+        element.addEventListener('click', function(){
+            const product = element.parentElement.parentElement.parentElement
+            product.remove()
+            priceBasket()
+        })
+    }
+
+    const basketContentBtnArr = document.querySelectorAll('.basket__content-btn')
+    for (let i = 0; i < basketContentBtnArr.length; i++) {
+        const element = basketContentBtnArr[i];
+        element.addEventListener('click', function(){
+            const basketContentChoice = element.parentElement
+            const basketContentNumber = basketContentChoice.querySelector('.basket__content-number')
+            const basketContentprice = basketContentChoice.parentElement.querySelector('.basket__content-price')
+            const price = Number(basketContentprice.textContent.slice(0, -1).replaceAll(' ', ''))
+            let basketQuantity = Number(basketContentNumber.textContent)
+            const pricePerUnit = price / basketQuantity
+            if (element.classList.contains('basket__content-minus')) {
+                basketContentNumber.textContent = basketQuantity - 1
+            }
+            if (element.classList.contains('basket__content-plus')) {
+                basketContentNumber.textContent = basketQuantity + 1
+            }
+            basketContentprice.textContent = `${(pricePerUnit * basketContentNumber.textContent).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ₽`
+            priceBasket()
+        })
+        
+    }
+})
