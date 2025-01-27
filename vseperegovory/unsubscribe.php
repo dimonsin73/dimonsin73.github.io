5ce6ebabe1057116246fb9f7fe9e601b604d7a37
dimonsin73 <dimonsin73@gmail.com>
2025-01-27 14:01:56 +0300

    vseperegovory

---
+++ vseperegovory/unsubscribe.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Отписка от рассылки</title>
+  </head>
+  
+  <style>
+  .h-100vh{height:100vh}
+  </style>
+  
+  <body class='h-100vh'>
+
+<div class='container d-flex align-items-center justify-content-center h-100'>
+<span class='fs-3'>Вы успешно отписались от рассылки.</span>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
