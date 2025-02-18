dbdfa0079c33055615eb4a1e43ace67d183d26e2
dimonsin73 <dimonsin73@gmail.com>
2025-02-18 19:13:36 +0300

    unordi 1.1

--- unordi/js/script.js
+++ unordi/js/script.js
@@ -10,10 +10,34 @@ const switchBtn = document.querySelector('.header__switch')
 const switchInput = document.querySelector('.header__input')
 switchBtn.addEventListener('click', function(){
     if (switchInput.checked) {
+        switchInput.checked = false;
+    } else {
+        switchInput.checked = true;
+    }
+    theme()
+})
+switchInput.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // код клавиши Enter
+        if (switchInput.checked) {
+            switchInput.checked = false;
+        } else {
+            switchInput.checked = true;
+        }
+        theme()
+    }
+})
+function theme() {
+    if (switchInput.checked) {
         body.classList.remove('light-theme')
         body.classList.add('dark-theme')
     } else {
         body.classList.add('light-theme')
         body.classList.remove('dark-theme')
     }
+}
+const x = document.getElementById('feedback-checkbox')
+const y = document.querySelector('.feedback__form-label')
+y.addEventListener('click', function(){
+    console.log(x.checked)
 })
