fdea0681d801c6ba694ad14a9c65d1af0d7b0364
dimonsin73 <dimonsin73@gmail.com>
2025-06-24 17:49:34 +0300

    ikingi 1.0.1

--- ikingi/js/script.js
+++ ikingi/js/script.js
@@ -15,6 +15,7 @@ const hiroImg = document.querySelector('.hiro__img')
 const menu = document.querySelector('.menu')
 const products = document.querySelector('.products')
 const modal = document.querySelector('.modal')
+const modalClose = document.querySelector('.modal__close')
 const modalWrapperArray = modal.querySelectorAll('.modal__wrapper')
 let deltaSumm = 0
 window.addEventListener('wheel', function(e) {
@@ -30,7 +31,6 @@ window.addEventListener('wheel', function(e) {
             if (deltaSumm > 400) {
                 deltaSumm = 400
             }
-            console.log(deltaSumm)
             if (deltaSumm > 0) {
                 hiro.style.backgroundSize = 'auto 125%'
                 hiroImg.style.height = '75%'
@@ -57,7 +57,6 @@ window.addEventListener('wheel', function(e) {
         }
     }
 });
-
 for (let i = 0; i < swiperScooterArray.length; i++) {
     const swiperScooter = swiperScooterArray[i];
     swiperScooter.addEventListener('click', function(){
@@ -79,5 +78,71 @@ for (let i = 0; i < swiperScooterArray.length; i++) {
                 modal.classList.remove('modal-active')
             }
         });
+        modalClose.addEventListener('click', function(){
+            for (let i = 0; i < modalWrapperArray.length; i++) {
+                const modalWrapper = modalWrapperArray[i];
+                modalWrapper.classList.remove('modal__wrapper-active')
+            }
+            modal.classList.remove('modal-active')
+        })
     })
 }
+
+let startX, startY;
+let sumDelta = 0
+window.addEventListener('touchstart', function(e) {
+    startX = e.touches[0].clientX;
+    startY = e.touches[0].clientY;
+});
+window.addEventListener('touchmove', function(e) {
+    let currentX = e.touches[0].clientX;
+    let currentY = e.touches[0].clientY;
+    let deltaX = currentX - startX;
+    let deltaY = currentY - startY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        if (deltaX > 0) {
+            // свайп вправо
+        } else {
+            // свайп влево
+        }
+    } else {
+        if (deltaY > 0) {
+            sumDelta++
+        } 
+        else {
+            sumDelta--
+        }
+    }
+});
+window.addEventListener('touchend', function(e) {
+    if (sumDelta > 0) {
+        sumDelta = 0
+    }
+    if (sumDelta < -80) {
+        sumDelta = -80
+    }
+    if (sumDelta < 0) {
+        hiro.style.backgroundSize = 'auto 125%'
+        hiroImg.style.height = '75%'
+        menu.style.top = '100%'
+        products.style.top = '100%'
+    }
+    if (sumDelta < -20) {
+        hiro.style.backgroundSize = 'auto 100%'
+        hiroImg.style.height = '100%'
+        menu.style.top = '50%'
+        hiro.style.top = '0%'
+        products.style.top = '100%'
+    }
+    if (sumDelta < -40) {
+            menu.style.top = '0'
+            hiro.style.top = '-100%'
+            products.style.top = '0%'
+    }
+    if (sumDelta < -60) {
+            menu.style.top = '0'
+            hiro.style.top = '-100%'
+            products.style.top = '0%'
+    }
+});
