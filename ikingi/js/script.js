9e64f627812d125b1c01c31936c9820f248986b7
dimonsin73 <dimonsin73@gmail.com>
2025-07-12 17:53:49 +0300

    ikingi 11.07.2025

--- ikingi/js/script.js
+++ ikingi/js/script.js
@@ -34,6 +34,7 @@ const swiperScooterArray = document.querySelectorAll('.swiper-scooter')
 const hiro = document.querySelector('.hiro')
 const hiroImg = document.querySelector('.hiro__img')
 const menu = document.querySelector('.menu')
+const nav = document.querySelector('.nav')
 const products = document.querySelector('.products')
 const modal = document.querySelector('.modal')
 const modalClose = document.querySelector('.modal__close')
@@ -42,38 +43,80 @@ let deltaSumm = 0
 window.addEventListener('wheel', function(e) {
     const withinBoundariesSW = e.composedPath().includes(swiperWrapper);
     const withinBoundariesM = e.composedPath().includes(modal);
+    const withinBoundariesI = e.composedPath().includes(info);
     if ( ! withinBoundariesSW ) {
         if ( ! withinBoundariesM ) {
-            const delta = e.deltaY;
-            deltaSumm = deltaSumm + delta
-            if (deltaSumm < 0) {
-                deltaSumm = 0
-            }
-            if (deltaSumm > 400) {
-                deltaSumm = 400
-            }
-            if (deltaSumm > 0) {
-                hiro.style.backgroundSize = 'auto 125%'
-                hiroImg.style.height = '75%'
-                menu.style.top = '100%'
-                products.style.top = '100%'
-            }
-            if (deltaSumm > 100) {
-                hiro.style.backgroundSize = 'auto 100%'
-                hiroImg.style.height = '100%'
-                menu.style.top = '50%'
-                hiro.style.top = '0%'
-                products.style.top = '100%'
-            }
-            if (deltaSumm > 200) {
+            if (! withinBoundariesI) {
+                const delta = e.deltaY;
+                deltaSumm = deltaSumm + delta
+                if (deltaSumm < 0) {
+                    deltaSumm = 0
+                }
+                if (deltaSumm > 900) {
+                    deltaSumm = 900
+                }
+                if (deltaSumm >= 0) {
+                    hiro.style.backgroundImage = "url(images/bg_1.png)"
+                    hiroImg.style.height = '60%'
+                    hiroImg.style.bottom = '300px'
+                    menu.style.top = '100%'
+                    products.style.top = '100%'
+                }
+                if (deltaSumm > 100) {
+                    hiro.style.backgroundImage = "url(images/bg_2.png)"
+                    hiroImg.style.height = '65%'
+                    hiroImg.style.bottom = '250px'
+                    menu.style.top = '90%'
+                }
+                if (deltaSumm > 200) {
+                    hiro.style.backgroundImage = "url(images/bg_3.png)"
+                    hiroImg.style.height = '70%'
+                    hiroImg.style.bottom = '200px'
+                    menu.style.top = '80%'
+                }
+                if (deltaSumm > 300) {
+                    hiro.style.backgroundImage = "url(images/bg_4.png)"
+                    hiroImg.style.height = '75%'
+                    hiroImg.style.bottom = '150px'
+                    menu.style.top = '70%'
+                }
+                if (deltaSumm > 400) {
+                    hiro.style.backgroundImage = "url(images/bg_5.png)"
+                    hiroImg.style.height = '80%'
+                    hiroImg.style.bottom = '100px'
+                    menu.style.top = '60%'
+                }
+                if (deltaSumm > 500) {
+                    hiro.style.backgroundImage = "url(images/bg_6.png)"
+                    hiroImg.style.height = '85%'
+                    hiroImg.style.bottom = '50px'
+                    menu.style.top = '50%'
+                }
+                if (deltaSumm > 600) {
+                    hiro.style.backgroundImage = "url(images/bg_7.png)"
+                    hiroImg.style.height = '90%'
+                    hiroImg.style.bottom = '0'
+                    menu.style.top = '40%'
+                }
+                if (deltaSumm > 700) {
+                    hiroImg.style.height = '80%'
+                    hiroImg.style.bottom = '0'
+                    menu.style.top = '30%'
+                    nav.classList.remove('nav-active')
+                    hiro.style.top = '0%'
+                    products.style.top = '100%'
+                }
+                if (deltaSumm > 800) {
+                    menu.style.top = '0'
+                    nav.classList.add('nav-active')
+                    hiro.style.top = '-100%'
+                    products.style.top = '0%'
+                }
+                if (deltaSumm >= 900) {
                     menu.style.top = '0'
                     hiro.style.top = '-100%'
                     products.style.top = '0%'
-            }
-            if (deltaSumm > 300) {
-                    menu.style.top = '0'
-                    hiro.style.top = '-100%'
-                    products.style.top = '0%'
+                }
             }
         }
     }
@@ -109,6 +152,33 @@ for (let i = 0; i < swiperScooterArray.length; i++) {
     })
 }
 
+const navListItemArray = document.querySelectorAll('.nav__list-item')
+const info = document.querySelector('.info')
+const infoWrapperArray = document.querySelectorAll('.info__wrapper')
+const infoClose = document.querySelector('.info__close')
+for (let i = 0; i < navListItemArray.length; i++) {
+    const navListItem = navListItemArray[i];
+    navListItem.addEventListener('click', function(){
+        info.classList.add('info-active')
+        const dataNav = navListItem.dataset.nav
+        for (let i = 0; i < infoWrapperArray.length; i++) {
+            const infoWrapper = infoWrapperArray[i];
+            if (infoWrapper.dataset.info === dataNav) {
+                infoWrapper.classList.add('info__wrapper-active')
+            } else {
+                infoWrapper.classList.remove('info__wrapper-active')
+            }
+        }
+        infoClose.addEventListener('click', function(){
+            info.classList.remove('info-active')
+        })
+        document.addEventListener('keydown', function(e) {
+            if( e.keyCode == 27 ){ 
+                info.classList.remove('info-active')
+            }
+        });
+    })
+}
 let startX, startY;
 let sumDelta = 0
 window.addEventListener('touchstart', function(e) {
@@ -137,33 +207,95 @@ window.addEventListener('touchmove', function(e) {
     }
 });
 window.addEventListener('touchend', function(e) {
-    if (sumDelta > 0) {
-        sumDelta = 0
+    if (deltaSumm > 0) {
+        deltaSumm = 0
     }
-    if (sumDelta < -60) {
-        sumDelta = -60
+    if (deltaSumm < -90) {
+        deltaSumm = -90
     }
-    if (sumDelta < 0) {
-        hiro.style.backgroundSize = 'auto 125%'
-        hiroImg.style.height = '75%'
+    if (deltaSumm >= 0) {
+        hiro.style.backgroundImage = "url(images/bg_1.png)"
+        hiroImg.style.height = '60%'
+        hiroImg.style.bottom = '300px'
         menu.style.top = '100%'
         products.style.top = '100%'
     }
-    if (sumDelta < -15) {
-        hiro.style.backgroundSize = 'auto 100%'
-        hiroImg.style.height = '100%'
+    if (deltaSumm > -10) {
+        hiro.style.backgroundImage = "url(images/bg_2.png)"
+        hiroImg.style.height = '65%'
+        hiroImg.style.bottom = '250px'
+        menu.style.top = '90%'
+    }
+    if (deltaSumm > -20) {
+        hiro.style.backgroundImage = "url(images/bg_3.png)"
+        hiroImg.style.height = '70%'
+        hiroImg.style.bottom = '200px'
+        menu.style.top = '80%'
+    }
+    if (deltaSumm > -30) {
+        hiro.style.backgroundImage = "url(images/bg_4.png)"
+        hiroImg.style.height = '75%'
+        hiroImg.style.bottom = '150px'
+        menu.style.top = '70%'
+    }
+    if (deltaSumm > -40) {
+        hiro.style.backgroundImage = "url(images/bg_5.png)"
+        hiroImg.style.height = '80%'
+        hiroImg.style.bottom = '100px'
+        menu.style.top = '60%'
+    }
+    if (deltaSumm > -50) {
+        hiro.style.backgroundImage = "url(images/bg_6.png)"
+        hiroImg.style.height = '85%'
+        hiroImg.style.bottom = '50px'
         menu.style.top = '50%'
+    }
+    if (deltaSumm > -60) {
+        hiro.style.backgroundImage = "url(images/bg_7.png)"
+        hiroImg.style.height = '90%'
+        hiroImg.style.bottom = '0'
+        menu.style.top = '40%'
+    }
+    if (deltaSumm > -70) {
+        hiroImg.style.height = '80%'
+        hiroImg.style.bottom = '0'
+        menu.style.top = '30%'
+        nav.classList.remove('nav-active')
         hiro.style.top = '0%'
         products.style.top = '100%'
     }
-    if (sumDelta < -30) {
-            menu.style.top = '0'
-            hiro.style.top = '-100%'
-            products.style.top = '0%'
+    if (deltaSumm > -80) {
+        menu.style.top = '0'
+        nav.classList.add('nav-active')
+        hiro.style.top = '-100%'
+        products.style.top = '0%'
     }
-    if (sumDelta < -45) {
-            menu.style.top = '0'
-            hiro.style.top = '-100%'
-            products.style.top = '0%'
+    if (deltaSumm >= -90) {
+        menu.style.top = '0'
+        hiro.style.top = '-100%'
+        products.style.top = '0%'
     }
 });
+
+const tabsBtnArray = document.querySelectorAll('.tabs__btn')
+for (let i = 0; i < tabsBtnArray.length; i++) {
+    const tabsBtn = tabsBtnArray[i];
+    tabsBtn.addEventListener('click', function(){
+        const tabsBtnArraySmall = tabsBtn.parentElement.querySelectorAll('.tabs__btn')
+        const modalContentArray = tabsBtn.parentElement.parentElement.querySelectorAll('.modal__content')
+        for (let i = 0; i < tabsBtnArraySmall.length; i++) {
+            const element = tabsBtnArraySmall[i];
+            element.classList.remove('tabs__btn-active')
+        }
+        tabsBtn.classList.add('tabs__btn-active')
+        const dataTabs = tabsBtn.dataset.tab 
+        for (let i = 0; i < modalContentArray.length; i++) {
+            const modalContent = modalContentArray[i];
+            if (modalContent.dataset.content === dataTabs) {
+                modalContent.classList.add('modal__content-active')
+            } else {
+                modalContent.classList.remove('modal__content-active')
+            }
+        }
+    })
+}
