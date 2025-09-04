885bea10560543cffd1f3e9c440e6875bb8218e5
dimonsin73 <dimonsin73@gmail.com>
2025-09-04 15:13:40 +0300

    кабинет

--- jobai/js/k_kabinet.js
+++ jobai/js/k_kabinet.js
@@ -128,3 +128,161 @@ for (let i = 0; i < clueArray.length; i++) {
         clueWrapper.classList.remove('clue__wrapper-active')
     })
 }
+
+const form = document.querySelector('.edit__form')
+// Поля ввода
+const password = document.getElementById('password')
+const passwordSecond = document.getElementById('password-repeat')
+const passwordLabel = password.parentElement
+const passwordSecondLabel = passwordSecond.parentElement
+
+
+// Проверка надёжности пароля
+password.addEventListener('input', function(){
+    const labelLine = password.nextSibling
+    const labelLineItemArr = labelLine.children
+    attributLineAddColor(password, labelLineItemArr)
+    passwordComparison()
+})
+passwordSecond.addEventListener('input', function(){
+    const labelLine = passwordSecond.nextSibling
+    const labelLineItemArr = labelLine.children
+    attributLineAddColor(passwordSecond, labelLineItemArr)
+    passwordComparison()
+})
+// Ф-ия очистки цветов
+function attributLineRemoveColor(element) {
+    element.classList.remove('label__line-orange')
+    element.classList.remove('label__line-success')
+    element.classList.remove('label__line-error')
+}
+// Ф-ция окрашивания 
+function attributLineAddColor(password, labelLineItemArr) {
+    if (password.value.length < 16) {
+        for (let i = 0; i < labelLineItemArr.length; i++) {
+            const element = labelLineItemArr[i];
+            attributLineRemoveColor(element)
+        }
+        for (let i = 0; i < labelLineItemArr.length; i++) {
+            const element = labelLineItemArr[i];
+            element.classList.add('label__line-success')
+        }
+    }
+    if (password.value.length < 14) {
+        for (let i = 0; i < labelLineItemArr.length; i++) {
+            const element = labelLineItemArr[i];
+            attributLineRemoveColor(element)
+        }
+        for (let i = 0; i < 3; i++) {
+            const element = labelLineItemArr[i];
+            element.classList.add('label__line-success')
+        }
+    }
+    if (password.value.length < 12) {
+        for (let i = 0; i < labelLineItemArr.length; i++) {
+            const element = labelLineItemArr[i];
+            attributLineRemoveColor(element)
+        }
+        for (let i = 0; i < 2; i++) {
+            const element = labelLineItemArr[i];
+            element.classList.add('label__line-orange')
+        }
+    }
+    if (password.value.length < 8) {
+        for (let i = 0; i < labelLineItemArr.length; i++) {
+            const element = labelLineItemArr[i];
+            attributLineRemoveColor(element)
+        }
+        for (let i = 0; i < 1; i++) {
+            const element = labelLineItemArr[i];
+            element.classList.add('label__line-error')
+        }
+    }
+    if (password.value.length === 0) {
+        for (let i = 0; i < labelLineItemArr.length; i++) {
+            const element = labelLineItemArr[i];
+            attributLineRemoveColor(element)
+        }
+    }
+}
+// Ф-ция проверки совпадения паролей
+function passwordComparison() {
+    
+    if (passwordSecond.value.length === 0) {
+        passwordSecondLabel.classList.remove('label_error')
+    } else {
+        if (passwordSecond.value != password.value) {
+            passwordSecondLabel.classList.add('label_error')
+            const labelLine = passwordSecond.nextSibling
+            const labelLineItemArr = labelLine.children
+            for (let i = 0; i < labelLineItemArr.length; i++) {
+                const element = labelLineItemArr[i];
+                attributLineRemoveColor(element)
+            }
+            for (let i = 0; i < 2; i++) {
+                const element = labelLineItemArr[i];
+                element.classList.add('label__line-error')
+            }
+        } else {
+            passwordSecondLabel.classList.remove('label_error')
+        }
+    }
+}
+// Открытие/скрытие пароля
+const labelEyeArray = document.querySelectorAll('.label__eye')
+for (let i = 0; i < labelEyeArray.length; i++) {
+    const labelEye = labelEyeArray[i];
+    labelEye.addEventListener('click', function(){
+        labelEye.classList.toggle('label__eye-active')
+        const password = labelEye.parentElement.querySelector('.input')
+        if (labelEye.classList.contains('label__eye-active')) {
+            password.setAttribute('type', 'text')
+        } else {
+            password.setAttribute('type', 'password')
+        }
+    })
+}
+const edit = document.querySelector('.edit')
+const editBtnArray = document.querySelectorAll('.edit-btn')
+const editFormArray = document.querySelectorAll('.edit__form')
+const editCloseArray = document.querySelectorAll('.edit__close')
+for (let i = 0; i < editBtnArray.length; i++) {
+    const editBtn = editBtnArray[i];
+    editBtn.addEventListener('click', function(){
+        const editData = editBtn.dataset.edit
+        edit.classList.add('edit_active')
+        for (let i = 0; i < editFormArray.length; i++) {
+            const editForm = editFormArray[i];
+            if (editForm.dataset.edit === editData) {
+                editForm.classList.add('edit__form-active')
+            } else {
+                editForm.classList.remove('edit__form-active')
+            }
+        }
+    })
+}
+for (let i = 0; i < editCloseArray.length; i++) {
+    const editClose = editCloseArray[i];
+    editClose.addEventListener('click', function(){
+        edit.classList.remove('edit_active')
+        for (let i = 0; i < editFormArray.length; i++) {
+            const editForm = editFormArray[i];
+            editForm.classList.remove('edit__form-active')
+        }
+    })
+}
+// Поднятия заголовка для импута 
+const labelInputArray = document.querySelectorAll('.label__input')
+for (let i = 0; i < labelInputArray.length; i++) {
+    const labelInput = labelInputArray[i];
+    labelInput.addEventListener('focus', function(){
+        const label = labelInput.parentElement
+        label.classList.add('label-active')
+    })
+    labelInput.addEventListener('focusout', function(){
+        if (labelInput.value === '') {
+            const label = labelInput.parentElement
+            label.classList.remove('label-active')
+        }
+    })
+}
