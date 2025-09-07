9c5bf28b9155ef3151f0cde2ed8f5ab79995b9dd
dimonsin73 <dimonsin73@gmail.com>
2025-09-07 11:00:36 +0300

    тест

---
+++ jobai/js/test.js
@@ -0,0 +1,24 @@
+const popupArray = document.querySelectorAll('.popup')
+for (let i = 0; i < popupArray.length; i++) {
+    const popup = popupArray[i];
+    const popupWrapper = popup.querySelector('.popup__wrapper')
+    if (window.innerWidth < '767') {
+        popup.addEventListener('click', function(){
+            popup.classList.add('popup_active')
+        })
+        document.addEventListener('click', (e) => {
+            const withinBoundaries = e.composedPath().includes(popupWrapper);
+            if ( ! withinBoundaries ) {
+                popup.classList.remove('popup_active')
+            }
+        })
+    } else {
+        popup.addEventListener('mouseenter', function(){
+            popup.classList.add('popup_active')
+        })
+        popup.addEventListener('mouseleave', function(){
+            popup.classList.remove('popup_active')
+        })
+    }
+    
+}
