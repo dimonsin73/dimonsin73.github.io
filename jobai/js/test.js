7e4b6a5325a302e2ac5188d95a52612025bceada
dimonsin73 <dimonsin73@gmail.com>
2025-09-07 12:11:39 +0300

    test

--- jobai/js/test.js
+++ jobai/js/test.js
@@ -30,3 +30,37 @@ for (let i = 0; i < neumorphicButtonArray.length; i++) {
         neumorphicButton.classList.toggle('neumorphic-button-active')
     })    
 }
+
+function registraciyaTimer(timer) {
+    const minutes = timer.querySelector('.timer__minutes')
+    const seconds = timer.querySelector('.timer__seconds')
+    let minutesValue =  Number(minutes.textContent)
+    let secondsValue = Number(seconds.textContent)
+    setInterval(function() {
+        if (secondsValue > 0) {
+            secondsValue--
+        } else {
+            if (minutesValue - 1 >= 0) {
+                minutesValue--
+                secondsValue = 60 - 1
+            } else {
+                clearInterval
+            }
+        }
+        if (secondsValue < 10) {
+            seconds.textContent = `0${secondsValue}`
+        } else {
+            seconds.textContent = secondsValue
+        }
+        if (minutesValue < 10) {
+            minutes.textContent = `0${minutesValue}`
+        } else {
+            minutes.textContent = minutesValue
+        }
+    }, 1000)
+}
+const timerArray = document.querySelectorAll('.timer')
+for (let i = 0; i < timerArray.length; i++) {
+    const timer = timerArray[i];
+    registraciyaTimer(timer)
+}
