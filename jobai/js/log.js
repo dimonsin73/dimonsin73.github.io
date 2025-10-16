0445f94547a7ea3d8c7bdf8589dba70535c9dff4
dimonsin73 <dimonsin73@gmail.com>
2025-10-16 21:30:54 +0300

    tariffs

--- jobai/js/log.js
+++ jobai/js/log.js
@@ -1,3 +1,65 @@
+// Работа подсказок 
+const clueArray = document.querySelectorAll('.clue')
+for (let i = 0; i < clueArray.length; i++) {
+    const clue = clueArray[i];
+    const clueText = clue.querySelector('.clue__text')
+    clue.addEventListener('mouseover', function(){
+        clueText.classList.add('clue__text-active')
+    })
+    clue.addEventListener('mouseout', function(){
+        clueText.classList.remove('clue__text-active')
+    })
+}
+// Открытие/Закрытие селектов
+const selectInputArray = document.querySelectorAll('.select__input')
+for (let i = 0; i < selectInputArray.length; i++) {
+    const selectInput = selectInputArray[i];
+    const selectTitle = selectInput.parentElement.querySelector('.select__title')
+    selectFun(selectInput, selectTitle)
+}
+function selectUp(selectInput, selectTitle) {
+    if (selectInput.value != '') {
+        selectTitle.classList.add('select__title-active')
+    }
+}
+// Функция открытие/Закрытие селектов
+function selectFun(selectInput, selectTitle) {
+    selectInput.addEventListener('click', function(){
+        const select = selectInput.parentElement
+        const options = selectInput.parentElement.querySelector('.options')
+        const optionArray = options.querySelectorAll('.option')
+        select.classList.toggle('select-active')
+        if (select.classList.contains('select-active')) {
+            options.classList.add('options_active')
+        } else {
+            options.classList.remove('options_active')
+        }
+        for (let i = 0; i < optionArray.length; i++) {
+            const option = optionArray[i];
+            option.addEventListener('click', function(){
+                selectInput.value = option.textContent
+                if (select.classList.contains('select_up')) {
+                    selectUp(selectInput, selectTitle)
+                }
+            options.classList.remove('options_active')
+                select.classList.remove('select-active')
+            })
+        }
+        document.addEventListener('click', (e) => {
+            const withinBoundaries = e.composedPath().includes(select);
+            if ( ! withinBoundaries ) {
+                select.classList.remove('select-active')
+                options.classList.remove('options_active')
+            }
+        }) //Закрытие селекта по щелчку вне меню
+        document.addEventListener('keydown', function(e) {
+            if( e.keyCode == 27 ){ 
+                select.classList.remove('select-active')
+                options.classList.remove('options_active')
+            }
+        }) //Закрытие селекта по нажатию на ESC
+    })
+}
 // Работа меню выбора языка 
 const language = document.querySelector('.language')
 const languageView = language.querySelector('.language__view')
@@ -80,8 +142,8 @@ for (let i = 0; i < popupArray.length; i++) {
             popup.classList.remove('popup_active')
         })
     }
-    
 }
+
 /* Переход к карточкам
 const cardsBtnArray = document.querySelectorAll('.cards-btn')
 const section = document.querySelector('.section')
@@ -303,3 +365,133 @@ documChatClose.addEventListener('click', function(){
     documChat.classList.remove('docum__chat-active')
     documChatbtn.style.display = 'block'
 })
+
+// Расчёт дней и стоимости
+const priceDay = 1666
+const documRangeArray = document.querySelectorAll('.docum__range')
+const documAccessInput = document.querySelector('.docum__access-input')
+const tariffsSale = document.getElementById('tariffs-sale')
+const tariffsPrice = document.getElementById('tariffs-price')
+const tariffsVat = document.getElementById('tariffs-vat')
+const tariffsDayPrice = document.getElementById('tariffs-day-price')
+const tariffsTotalPrice = document.getElementById('tariffs-total-price')
+const tariffsTotalVat = document.getElementById('tariffs-total-vat')
+const additionalRecruiters = document.getElementById('additional-recruiters')
+const percentageOfCost = document.getElementById('percentage-of-cost')
+const paymentTotalPrice = document.getElementById('payment-total-price')
+const paymentTotalVat = document.getElementById('payment-total-vat')
+
+for (let i = 0; i < documRangeArray.length; i++) {
+    const documRange = documRangeArray[i];
+    const documRangeInput = documRange.querySelector('.range__input')
+    const documRangeMeaning = documRange.querySelector('.range__meaning')
+    let documRangeMeaningPosition = documRangeInput.value * 100 / documRangeInput.max
+    documRangeMeaning.style.left = `${documRangeMeaningPosition}%`
+    documRangeInput.addEventListener('input', function(){
+        documRangeMeaning.textContent = documRangeInput.value
+        let documRangeMeaningPosition = documRangeInput.value * 100 / documRangeInput.max
+        documRangeMeaning.style.left = `${documRangeMeaningPosition}%`
+        documAccessInputNum ()
+    })   
+}
+const additionalRecruitersOptionArray = additionalRecruiters.parentElement.querySelectorAll('.option')
+let risePrice = 1
+let sale = 1
+for (let i = 0; i < additionalRecruitersOptionArray.length; i++) {
+    const additionalRecruitersOption = additionalRecruitersOptionArray[i];
+    additionalRecruitersOption.addEventListener('click', function(){
+        switch (additionalRecruitersOption.dataset.recruiters) {
+            case "1":
+                percentageOfCost.value = '0%'
+                risePrice = 1
+                break;
+            case "2":
+                percentageOfCost.value = '+ 5%'
+                risePrice = 1.05
+                break;
+            case "3":
+                percentageOfCost.value = '+ 10%'
+                risePrice = 1.1
+                break;
+            case "4":
+                percentageOfCost.value = '+ 15%'
+                risePrice = 1.15
+                break;
+            default:
+                break;
+        }
+        totalPrice(documAccessInput.value)
+    })
+}
+
+
+function documAccessInputNum (){
+    const rangeDays =  document.getElementById('range-days')
+    const rangeDaysValue = rangeDays.value
+    const rangeDaysMeaning = rangeDays.parentElement.querySelector('.range__meaning')
+    const rangeMonthsValue = document.getElementById('range-months').value
+    if (Number(rangeMonthsValue) === 12) {
+        rangeDays.value = 0
+        rangeDaysMeaning.textContent = 0
+        rangeDaysMeaning.style.left = 0
+        rangeDays.disabled = true
+        rangeDays.classList.add('range__input-disabled')
+        documAccessInput.value = 360
+        totalPrice(documAccessInput.value)
+    } else {
+        rangeDays.disabled = false
+        rangeDays.classList.remove('range__input-disabled')
+        documAccessInput.value = Number(rangeDaysValue) + Number(rangeMonthsValue)*30
+        totalPrice(documAccessInput.value)
+    }
+}
+function totalPrice(days){
+    if (days < 31) {
+        tariffsSale.value = 'Без скидок'
+        sale = 1
+    }
+    if (days > 30) {
+        tariffsSale.value = 'Скидка 5%'
+        sale = 0.95
+    }
+    if (days > 90) {
+        tariffsSale.value = 'Скидка 10%'
+        sale = 0.9
+    }
+    if (days > 180) {
+        tariffsSale.value = 'Скидка 20%'
+        sale = 0.8
+    }
+    
+    tariffsDayPrice.value = (priceDay * sale * risePrice).toFixed(2)
+    tariffsPrice.value = (days * priceDay * sale * risePrice).toFixed(2)
+    tariffsTotalPrice.value = tariffsPrice.value
+    
+    tariffsVat.value = ( (tariffsPrice.value * 20/100) / (1 + 20/100) ).toFixed(2)
+    tariffsTotalVat.value = ( (tariffsTotalPrice.value * 20/100) / (1 + 20/100) ).toFixed(2)
+}
+
+const tariffsCurrency = document.getElementById('tariffs-currency')
+const tariffsCurrencyOptionArray = tariffsCurrency.parentElement.querySelectorAll('.option')
+const labelCurrencyArray = tariffsCurrency.parentElement.parentElement.querySelectorAll('.label__currency')
+for (let i = 0; i < tariffsCurrencyOptionArray.length; i++) {
+    const tariffsCurrencyOption = tariffsCurrencyOptionArray[i];
+    tariffsCurrencyOption.addEventListener('click', function(){
+        for (let i = 0; i < labelCurrencyArray.length; i++) {
+            const labelCurrency = labelCurrencyArray[i];
+            labelCurrency.textContent = tariffsCurrencyOption.textContent
+        }
+    })
+}
+const tariffsCurrencyTotal = document.getElementById('tariffs-currency-total')
+const tariffsCurrencyTotalArray = tariffsCurrencyTotal.parentElement.querySelectorAll('.option')
+const labelCurrencyTotalArray = tariffsCurrencyTotal.parentElement.parentElement.querySelectorAll('.label__currency')
+for (let i = 0; i < tariffsCurrencyTotalArray.length; i++) {
+    const tariffsCurrencyTotal = tariffsCurrencyTotalArray[i];
+    tariffsCurrencyTotal.addEventListener('click', function(){
+        for (let i = 0; i < labelCurrencyTotalArray.length; i++) {
+            const labelCurrencyTotal = labelCurrencyTotalArray[i];
+            labelCurrencyTotal.textContent = tariffsCurrencyTotal.textContent
+        }
+    })
+}
