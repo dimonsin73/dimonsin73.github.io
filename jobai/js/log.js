d47a033565f9e45a3792b484170f474053307bb1
dimonsin73 <dimonsin73@gmail.com>
2025-10-16 21:44:45 +0300

    tariffs

--- jobai/js/log.js
+++ jobai/js/log.js
@@ -2,12 +2,12 @@
 const clueArray = document.querySelectorAll('.clue')
 for (let i = 0; i < clueArray.length; i++) {
     const clue = clueArray[i];
-    const clueText = clue.querySelector('.clue__text')
+    const clueWrapper = clue.querySelector('.clue__wrapper')
     clue.addEventListener('mouseover', function(){
-        clueText.classList.add('clue__text-active')
+        clueWrapper.classList.add('clue__wrapper-active')
     })
     clue.addEventListener('mouseout', function(){
-        clueText.classList.remove('clue__text-active')
+        clueWrapper.classList.remove('clue__wrapper-active')
     })
 }
 // Открытие/Закрытие селектов
