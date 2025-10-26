ee3b010ac1b82af9fb8896f86238a94c21811698
dimonsin73 <dimonsin73@gmail.com>
2025-10-26 10:53:26 +0300

    тарифы

--- jobai/js/tariffs_uspekh.js
+++ jobai/js/tariffs_uspekh.js
@@ -10,3 +10,20 @@ for (let i = 0; i < clueArray.length; i++) {
         clueWrapper.classList.remove('clue__wrapper-active')
     })
 }
+
+
+const tariffsFormArray = document.querySelectorAll('.tariffs__form')
+const tariffsBannerRabotodatel = document.querySelector('.tariffs__banner_rabotodatel')
+
+for (let i = 0; i < tariffsFormArray.length; i++) {
+    const tariffsForm = tariffsFormArray[i];
+    tariffsForm.addEventListener('scroll', function(){
+        if (tariffsForm.scrollTop > 0) {
+            tariffsBannerRabotodatel.classList.add('tariffs__banner-min')
+            tariffsForm.classList.add('tariffs__form-max')
+        } else {
+            tariffsBannerRabotodatel.classList.remove('tariffs__banner-min')
+            tariffsForm.classList.remove('tariffs__form-max')
+        }
+    })
+}
