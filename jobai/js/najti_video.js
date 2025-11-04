647fd4c385fcd3093318a5bbc387e189793e8e92
dimonsin73 <dimonsin73@gmail.com>
2025-11-05 01:18:39 +0300

    Найти

--- jobai/js/najti_video.js
+++ jobai/js/najti_video.js
@@ -29,47 +29,81 @@ headerBurger.addEventListener('click', function(){
         }
     }) //Скрытие Aside по нажатию на ESC
 })
-const btnTranscription = document.querySelector('.btn-transcription')
-const btnStructuring = document.querySelector('.btn-structuring')
+const btnTranscriptionArray = document.querySelectorAll('.btn-transcription')
+const btnStructuringArray = document.querySelectorAll('.btn-structuring')
+const btnSubvideoArray = document.querySelectorAll('.btn-subvideo')
+
 const subvideoContentArray = document.querySelectorAll('.subvideo__content')
 const sectionBtnsWidth = document.querySelector('.section__btns-width')
-const btnSubvideo = document.querySelector('.btn-subvideo')
 const main = document.querySelector('.main')
 const sectionMain = document.querySelector('.section__main')
 const subvideo = document.querySelector('.subvideo')
 const containerVideo = document.querySelector('.container_video')
-btnSubvideo.addEventListener('click', function(){
-    containerVideo.classList.remove('container_video-active')
-    btnSubvideo.classList.remove('btn-subvideo-active')
-    sectionBtnsWidth.classList.remove('section__btns-width-active')
-    subvideo.classList.remove('subvideo_active')
-    sectionMain.classList.remove('section__main-active')
-    main.classList.remove('main_active')
-    aside.classList.remove('aside_off')
-    btnTranscription.classList.remove('btn_white-active')
-    btnTranscription.classList.add('btn-icon48')
-    btnStructuring.classList.remove('btn_white-active')
-    btnStructuring.classList.add('btn-icon48')
-})
-btnTranscription.addEventListener('click', function(){
-    openSubVideo ()
-    btnTranscription.classList.add('btn_white-active')
-    btnTranscription.classList.remove('btn-icon48')
-    btnStructuring.classList.remove('btn_white-active')
-    btnStructuring.classList.add('btn-icon48')
-    openSubvideoConten('transcription')
-})
-btnStructuring.addEventListener('click', function(){
-    openSubVideo ()
-    btnStructuring.classList.add('btn_white-active')
-    btnStructuring.classList.remove('btn-icon48')
-    btnTranscription.classList.remove('btn_white-active')
-    btnTranscription.classList.add('btn-icon48')
-    openSubvideoConten('structuring')
-})
+
+for (let i = 0; i < btnSubvideoArray.length; i++) {
+    const btnSubvideo = btnSubvideoArray[i];
+    btnSubvideo.addEventListener('click', function(){
+        containerVideo.classList.remove('container_video-active')
+        btnSubvideo.classList.remove('btn-subvideo-active')
+        sectionBtnsWidth.classList.remove('section__btns-width-active')
+        subvideo.classList.remove('subvideo_active')
+        sectionMain.classList.remove('section__main-active')
+        main.classList.remove('main_active')
+        aside.classList.remove('aside_off')
+        for (let i = 0; i < btnTranscriptionArray.length; i++) {
+            const btnTranscription = btnTranscriptionArray[i];
+            btnTranscription.classList.remove('btn_white-active')
+            btnTranscription.classList.add('btn-icon48')
+        }
+        for (let i = 0; i < btnStructuringArray.length; i++) {
+            const btnStructuring = btnStructuringArray[i];
+            btnStructuring.classList.remove('btn_white-active')
+            btnStructuring.classList.add('btn-icon48')
+        }
+    })
+}
+for (let i = 0; i < btnTranscriptionArray.length; i++) {
+    const btnTranscription = btnTranscriptionArray[i];
+    btnTranscription.addEventListener('click', function(){
+        openSubVideo ()
+        for (let i = 0; i < btnTranscriptionArray.length; i++) {
+            const btnTranscription = btnTranscriptionArray[i];
+            btnTranscription.classList.add('btn_white-active')
+            btnTranscription.classList.remove('btn-icon48')
+        }
+        for (let i = 0; i < btnStructuringArray.length; i++) {
+            const btnStructuring = btnStructuringArray[i];
+            btnStructuring.classList.remove('btn_white-active')
+            btnStructuring.classList.add('btn-icon48')
+        }
+        openSubvideoConten('transcription')
+    })
+}
+
+for (let i = 0; i < btnStructuringArray.length; i++) {
+    const btnStructuring = btnStructuringArray[i];
+    btnStructuring.addEventListener('click', function(){
+        openSubVideo ()
+        for (let i = 0; i < btnTranscriptionArray.length; i++) {
+            const btnTranscription = btnTranscriptionArray[i];
+            btnTranscription.classList.remove('btn_white-active')
+            btnTranscription.classList.add('btn-icon48')
+        } 
+        for (let i = 0; i < btnStructuringArray.length; i++) {
+            const btnStructuring = btnStructuringArray[i];
+            btnStructuring.classList.add('btn_white-active')
+            btnStructuring.classList.remove('btn-icon48')
+        }
+        openSubvideoConten('structuring')
+    })
+}
+
 function openSubVideo () {
     containerVideo.classList.add('container_video-active')
-    btnSubvideo.classList.add('btn-subvideo-active')
+    for (let i = 0; i < btnSubvideoArray.length; i++) {
+        const btnSubvideo = btnSubvideoArray[i];
+        btnSubvideo.classList.add('btn-subvideo-active')
+    }
     sectionBtnsWidth.classList.add('section__btns-width-active')
     subvideo.classList.add('subvideo_active')
     sectionMain.classList.add('section__main-active')
